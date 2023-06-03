f7155680b2a944edd21118de7095b7124767c44a
Lennart Hennigs <mail@lennarthennigs.de>
2023-06-03 10:45:56 +0200

    made all UID parts 2 digits long

--- rc522.js
+++ rc522.js
@@ -1,5 +1,4 @@
 module.exports = function(RED) {
-
   function rc522Node(config) {
     RED.nodes.createNode(this, config);
     this.blockedFor = config.blockedFor;
@@ -8,10 +7,10 @@ module.exports = function(RED) {
     const Mfrc522 = require("../mfrc522-rpi/index");
     const SoftSPI = require("../rpi-softspi/index");
     const softSPI = new SoftSPI({
-      clock:  23, // pin number of SCLK
-      mosi:   19, // pin number of MOSI
-      miso:   21, // pin number of MISO
-      client: 24  // pin number of CS
+      clock:  23,
+      mosi:   19,
+      miso:   21,
+      client: 24
     });
     const mfrc522 = new Mfrc522(softSPI).setResetPin(22);
 
@@ -21,51 +20,38 @@ module.exports = function(RED) {
 
     setInterval(function() {
       const timestamp = Date.now();
-      if (timestamp > (last_time + node.blockedFor * 1000)) {
+      const nextTime = last_time + node.blockedFor * 1000;
+      if (timestamp > nextTime) {
         node.status({});
       }
       mfrc522.reset();
-      let response = mfrc522.findCard();
-      if (response.status) {
-        const bitSize = response.bitSize;
-        response = mfrc522.getUid();
-        if (response.status) {
-          const uid_array = response.data;
-          let uid = ""
-          for(var i = 0; i < uid_array.length;i++){
-                if(i > 0){
-                  uid += ":"
-                }
-                if(uid_array[i] < 10){
-                  uid += "0"
-                }
-                uid += uid_array[i].toString(16).toUpperCase()
-          }
-            if  (uid != last_uid || timestamp > (last_time + node.blockedFor * 1000)) {
-              node.status({
-                fill: "green",
-                shape: "dot",
-                text: uid
-              });
-              last_time = timestamp;
-              last_uid = uid;
-               node.send({"payload": {
-                 "uid": uid,
-                 "timestamp": timestamp,
-//                 "uid_array": uid_array,
-                 "bitSize": bitSize
-               }});
+      let cardFindResponse = mfrc522.findCard();
+      if (cardFindResponse.status) {
+        const bitSize = cardFindResponse.bitSize;
+        let cardUidResponse = mfrc522.getUid();
+        if (cardUidResponse.status) {
+          let uid = cardUidResponse.data.map(val => val.toString(16).padStart(2, '0')).join(':').toUpperCase();
 
-  node.log("New Card detected, CardType: " + bitSize);
-  node.log("UID: " + uid);
+          if  (uid !== last_uid || timestamp > nextTime) {
+            node.status({
+              fill: "green",
+              shape: "dot",
+              text: uid
+            });
+            last_time = timestamp;
+            last_uid = uid;
+            node.send({"payload": {
+              "uid": uid,
+              "timestamp": timestamp,
+              "bitSize": bitSize
+            }});
 
+            node.log(`New Card detected, CardType: ${bitSize}`);
+            node.log(`UID: ${uid}`);
           }
         } else {
-         node.log("Card read error!");
-         return;
+          node.log("Card read error!");
         }
-      } else {
-        return;
       }
     }, refreshRate * 1000);
   }
